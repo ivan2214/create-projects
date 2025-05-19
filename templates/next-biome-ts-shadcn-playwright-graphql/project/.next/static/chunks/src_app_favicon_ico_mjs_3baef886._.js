f3b716b273a66046c112ba8be0a8ad818155fd65
ivan2214 <bongiovanniivan12@gmail.com>
2025-05-19 17:17:19 -0300

    fix: remove .next

--- templates/next-biome-ts-shadcn-playwright-graphql/project/.next/static/chunks/src_app_favicon_ico_mjs_3baef886._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/bceaa_next_dist_cbf2f578._.js"
-],
-    source: "dynamic"
-});
